e10e36f1a78933f6ab55c1286909ef3c57e57d42
epolak01 <56269269+epolak01@users.noreply.github.com>
2020-04-28 19:59:57 -0400

    Add files via upload

--- database.js
+++ database.js
@@ -13,6 +13,7 @@ var port = process.env.PORT || 3000;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write("Hello World")
 
     //Parse the server request
     var q = url.parse(req.url, true).query;
